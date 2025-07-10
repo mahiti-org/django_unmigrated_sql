42215a9081d7820de40d85241e547af9ce5e2d24
Sreekanth S Rameshaiah <sree@mahiti.org>
2025-07-10 17:34:58 +0530

    bump up the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-unmigrated-sql",
-    version="0.1.0",
+    version="0.2.0",
     description="A Django management command to execute and fake all unmigrated SQL migrations for any or all apps, ignoring errors and not using transactions.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
